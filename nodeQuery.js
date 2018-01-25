e954654c6e7c64215970076b0eea9c11a5ad696c
subhranshudas <subhranshu.opt@gmail.com>
2018-01-25 12:54:25 -0500

    a simple example which shows how to wrap https module get functino with native promise and write synchronous code using async, await

---
+++ nodeQuery.js
@@ -0,0 +1,84 @@
+const https = require('https');
+const apiBaseUrl = `https://jsonmock.hackerrank.com/api/movies/search/Title?=`;
+
+function httpRequestWrapper(url) {
+    return new Promise(function(resolve, reject) { // this is where we promisify
+        var req = https.request(url, function(res) {
+            // reject on bad status
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+            // cumulate data
+            var body = [];
+            res.on('data', function(chunk) {
+                body.push(chunk);
+            });
+            // resolve on end
+            res.on('end', function() {
+                try {
+                    body = JSON.parse(Buffer.concat(body).toString());
+                } catch(e) {
+                    reject(e); // this is where we kinda return our HTTP call result but only on 'end'
+                }
+                resolve(body); // this is where we kinda return our HTTP call result but only on 'end'
+            });
+        });
+        // reject on request error
+        req.on('error', function(err) {
+            // This is not a "Second reject", just a different sort of failure
+            console.log('something went wrong: \n', err);
+            reject(err);
+        });
+        // IMPORTANT
+        req.end();
+    });
+}
+
+async function promiseAll (promises) {
+    return Promise.all(promises)
+        .then(results => results)
+        .catch(error => {
+            console.log('something went wrong in promiseAll: ', error);
+        });
+}
+
+async function getMovies (query) {
+    const moviesApiUrl = `${apiBaseUrl}${query}`;
+    return httpRequestWrapper(moviesApiUrl)
+        .then(movies => movies);
+}
+
+async function getAllMovies (moviesPages) {
+    let allMovies = await promiseAll(moviesPages);
+    let moviesInTotal = [];
+    allMovies
+        .map(({data}) => data)
+        .forEach(movies => {
+            const titlesPerPage =  movies.map(({Title}) => Title)
+            moviesInTotal = [...moviesInTotal, ...titlesPerPage];
+        })
+    return moviesInTotal;
+}
+
+
+async function getMoviesTitles (query) {
+    let movies = await getMovies(query);
+    const totalPages = movies.total_pages;
+        
+    let moviesPages = [...new Array(totalPages)].map((noop, index) => {
+        const moviesPageApiUrl = `${apiBaseUrl}${query}&page=${index + 1}`;
+        return httpRequestWrapper(moviesPageApiUrl);
+    });
+
+    let moviesList = await getAllMovies(moviesPages)
+
+    return moviesList
+       .filter(title => title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+}
+
+getMoviesTitles('spiderman')
+    .then(titles => {
+        console.log('printing the results: \n\n', titles);
+    });
+
+
